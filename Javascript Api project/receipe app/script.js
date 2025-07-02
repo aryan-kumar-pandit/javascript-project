1be4c358d8b94f77f040a120859d12f5b9465847
aryan-kumar-pandit <aryankumarpandito@gmail.com>
2025-07-02 14:08:00 +0530

    js added for recipe

--- Javascript Api project/receipe app/script.js
+++ Javascript Api project/receipe app/script.js
@@ -4,18 +4,25 @@ const receipeContainer = document.querySelector('.receipe-container');
 
 const fetchRecipes = async (query)=>{
 
+    receipeContainer.innerHTML = "<h2>Fetching recipe ...</h2>";
     const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response = await data.json();
+    
+    receipeContainer.innerHTML = "";
     response.meals.forEach(meal => {
         //console.log(meal);
         const recipeDiv = document.createElement('div');
         recipeDiv.classList.add('recipe');
+
         recipeDiv.innerHTML = `
         <img src="${meal.strMealThumb}">
         <h3>${meal.strMeal}</h3>
-        <p>${meal.strArea}<p/>
-        <p>${meal.strCategory}<p/>
-        `
+        <p><span>${meal.strArea}</span> Dish<p/>
+        <p>Belongs to <span>${meal.strCategory}</span> Category<p/>`;
+
+        const button = document.createElement('button');
+        button.textContent = "View Recipe";
+        recipeDiv.appendChild(button);
         receipeContainer.appendChild(recipeDiv);
         
     });
