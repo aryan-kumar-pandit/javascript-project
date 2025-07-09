49f61e62ac0c848ee5a66649d63406b8aaacfbbb
aryan-kumar-pandit <aryankumarpandito@gmail.com>
2025-07-09 10:38:23 +0530

    dictionary app complete

--- Javascript Api project/dictionary app/script.js
+++ Javascript Api project/dictionary app/script.js
@@ -1,1 +1,45 @@
-const form = document.querySelector('form');
+const form = document.querySelector("form");
+const resultDiv = document.querySelector(".result");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getWordInfo(form.elements[0].value);
+});
+
+//use free dictionary api
+const getWordInfo = async (word) => {
+  try {
+    resultDiv.innerHTML="Fetching Data ...";
+  const response = await fetch(
+    `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+  );
+  const data = await response.json();
+  let definitions = data[0].meanings[0].definitions[0];
+
+  resultDiv.innerHTML = `
+    <h2><strong>Word: </strong>${data[0].word}</h2>
+    <p class="partOfSpeech">${data[0].meanings[0].partOfSpeech}</p>
+    <p><strong>Meaning: </strong>${
+      definitions.definition === undefined
+        ? "Not Found"
+        : definitions.definition
+    }</p>
+    <p><strong>Meaning: </strong>${
+      definitions.example === undefined ? "Not Found" : definitions.example
+    }</p>
+    <p><strong>Antonyms: </strong></p>
+    `;
+  if (definitions.antonyms.length === 0) {
+    resultDiv.innerHTML += `<span>Not Fouund</span>`;
+  } else {
+    for (let i = 0; i < definitions.antonyms.length; i++) {
+      resultDiv.innerHTML += `<li>${definitions.antonyms[i]}</li>`;
+    }
+  }
+  //Adding Read More Button
+  resultDiv.innerHTML += `<div><a href="${data[0].sourceUrls}" target="_blank">Read More</a></div>`;
+
+  } catch (error) {
+    resultDiv.innerHTML = `<p>Sorry Result Not Found !!!</p>`;
+  }
+};
